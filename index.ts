3cc895ab2259a6eda1e109ff0a359580880af2c6
navani_hk <navaneetha2006krishnan@gmail.com>
2025-02-28 11:24:27 +0530

    ...

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
-const fastify = require('fastify')();
-const PORT = 4000;
+import Fastify from 'fastify'
 
-fastify.get('/', async (req:any, reply:any) => {
-  return { message: 'Welcome, your app is working well' };
-});
 
-fastify.listen(PORT, (err:any, address:any) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', async (request:any, reply:any) => {
+  return { hello: 'world' }
+})
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
   }
-  console.log(`Server running at ${address}`);
-});
-
-// Export the Fastify API
-module.exports = fastify;
+}
+start()
